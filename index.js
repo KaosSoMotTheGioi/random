5d53a0985174970ed5adbc6d1e2b00c3c4e2db85
KaosSoMotTheGioi <thanhdatpro071203@gmail.com>
2024-09-05 13:06:54 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function randomRedirect() {
+    const urls = [
+      "https://github.com/KaosSoMotTheGioi/Image-host/blob/main/IMG/image1.gif?raw=true",
+      "https://github.com/KaosSoMotTheGioi/Image-host/blob/main/IMG/03.gif?raw=true",
+      "https://github.com/KaosSoMotTheGioi/Image-host/blob/main/IMG/pp_arknights240730.gif?raw=true",
+      "https://github.com/KaosSoMotTheGioi/Image-host/blob/main/IMG/nkmy_02.gif?raw=true",
+      "https://github.com/KaosSoMotTheGioi/Image-host/blob/main/IMG/cs.gif?raw=true",
+    ];
