03233e0936b5dc89b7c0b2aedbe447a74deb1fd3
KaosSoMotTheGioi <thanhdatpro071203@gmail.com>
2024-09-05 13:09:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ function randomRedirect() {
     const urls = [
       "https://github.com/KaosSoMotTheGioi/Image-host/blob/main/IMG/image1.gif?raw=true",
       "https://github.com/KaosSoMotTheGioi/Image-host/blob/main/IMG/03.gif?raw=true",
-      "https://github.com/KaosSoMotTheGioi/Image-host/blob/main/IMG/pp_arknights240730.gif?raw=true",
-      "https://github.com/KaosSoMotTheGioi/Image-host/blob/main/IMG/nkmy_02.gif?raw=true",
-      "https://github.com/KaosSoMotTheGioi/Image-host/blob/main/IMG/cs.gif?raw=true",
+      "https://google.com",
     ];
+  
+    const randomIndex = Math.floor(Math.random() * urls.length);
+    const randomUrl = urls[randomIndex];
+  
+    const metaTag = document.createElement('meta');
+    metaTag.httpEquiv = 'refresh';
+    metaTag.content = `0; url=${randomUrl}`;
+    document.head.appendChild(metaTag);
+  }
+  
+
+  randomRedirect();
